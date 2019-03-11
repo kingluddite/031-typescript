c828d80d81e30276ed92ebd0e8cc0475a7bb0cd1
Kingluddite <howley.phil@gmail.com>
2019-03-10 23:27:04 -0700

    Add types for typescript

---
+++ index.ts
@@ -0,0 +1,18 @@
+const isOpen: boolean = true;
+const myName: string = 'John';
+const myAge: number = 6;
+const myFavoriteDecimal: number = 6.03;
+// number in an array
+const list: number[] = [0, 1, 2];
+// tupal
+const you: [string, number, boolean] = ['John', 33, true];
+// enum (think of it as a list)
+enum Job {
+  WebDev,
+  WebDesigner,
+  PM
+}
+const job: Job = Job.WebDev;
+
+const phone: any = 'Phone';
+const tablet: any = 3;
